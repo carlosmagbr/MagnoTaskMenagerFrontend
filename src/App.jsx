b4e530980752f3a2fa03ac83d0e2447789d2fc52
carlosmagno02 <carlosmabr44@gmail.com>
2025-04-09 00:32:04 -0300

    feat: add Login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.scss'
 import Home from './page/Home';
+import Login from './page/Login';
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
         </Routes>
     );
 };
---
+++ src/page/Login.jsx
@@ -0,0 +1,16 @@
+import CustomButton from "../components/CustomButton";
+import "./Login.scss";
+
+const Login = () => {
+    <div className="login-container">
+        <div className="title">
+            <h1>Magno's</h1>
+            <p>TaskMenages</p>
+        </div>
+        <div className="button-container">
+            <CustomButton> Entrar </CustomButton>
+        </div>
+    </div>;
+};
+
+export default Login;
