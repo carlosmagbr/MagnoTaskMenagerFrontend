734712bf4b0a0a57c3cea421a9fd1424af2e8b5d
carlosmagno02 <carlosmabr44@gmail.com>
2025-04-09 00:39:20 -0300

    feat: add login route

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -2,15 +2,17 @@ import CustomButton from "../components/CustomButton";
 import "./Login.scss";
 
 const Login = () => {
-    <div className="login-container">
-        <div className="title">
-            <h1>Magno's</h1>
-            <p>TaskMenages</p>
+    return (
+        <div className="login-container">
+            <div className="title">
+                <h1>Magno's</h1>
+                <p>TaskMenager</p>
+            </div>
+            <div className="button-container">
+                <CustomButton> Entrar </CustomButton>
+            </div>
         </div>
-        <div className="button-container">
-            <CustomButton> Entrar </CustomButton>
-        </div>
-    </div>;
+    );
 };
 
 export default Login;
